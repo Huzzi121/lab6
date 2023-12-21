86ca1b51b663158d7077967290cd3e5e9240be6a
huzaifa <huzaifakashif2003@gmail.com>
2023-12-21 13:01:01 +0500

    variable per star lagana bhool gaya tha

--- colorUtils.c
+++ colorUtils.c
@@ -11,26 +11,28 @@ int min(int a, int b, int c) {
   return (a < b) ? (a < c ? a : c) : (b < c ? b : c);
 }
 
-ErrorCode toGrayScale(int *r, int *g, int *b, Mode mode) {
+ErrorCode toGrayScale(int *r, int *g, int *b, Mode mode)//int per error aa raha tha 
+ {
   //TODO: implement
-  if(r<0 || g<0 || b<0 || r>255 || g>255 || b>255)
+  if(*r<0 || *g<0 || *b<0 || *r>255 || *g>255 || *b>255)
   {
     return INVALID_INPUT;
   }
-  int average=(r+g+b)/3;
+  int average=(*r+*g+*b)/3;
 
   if(mode==average || mode==LIGHTNESS)
   {
-    r=g=b=average;
+    *r=*g=*b=average;
   }
   else if(mode==LUMINOSITY)
   {
-    r=g=b=(int)(0.21*r+0.72*g+0.07*b);
+    *r=*g=*b=(int)(0.21**r+0.72**g+0.07**b);
   }
 
   return NO_ERROR;
 }
 
-int toSepia(int *r, int *g, int *b) {
+ErrorCode toSepia(int *r, int *g, int *b) {
   //TODO: implement
+  if()
 }
